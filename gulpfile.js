984429b6ac2cc8be00abe763cfbc69ea788cf5c8
oleob <oleob@stud.ntnu.no>
2017-06-10 23:42:56 +0200

    added bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,22 @@ var browserSync = require('browser-sync');
 var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
+
+var config = {
+	port: 9005,
+	devBaseUrl: 'http://localhost',
+	paths: {
+		html: './src/*.html',
+		js: './src/**/*.js',
+		css: [
+      		'node_modules/bootstrap/dist/css/bootstrap.min.css',
+      		'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+    	],
+		dist: './.tmp',
+		mainJs: './src/main.js'
+	}
+}
 
 gulp.task('live-server', function () {
   var server = new LiveServer('server/main.js');
@@ -18,12 +34,18 @@ gulp.task('bundle', function () {
   .transform(reactify)
   .bundle()
   .pipe(source('app.js'))
-  .pipe(gulp.dest('./.tmp'));
+  .pipe(gulp.dest(config.paths.dist));
 });
 
-gulp.task('serve', ['bundle','live-server'], function () {
+gulp.task('css', function() {
+	gulp.src(config.paths.css)
+		.pipe(concat('bundle.css'))
+		.pipe(gulp.dest(config.paths.dist + '/css'));
+});
+
+gulp.task('serve', ['css','bundle','live-server'], function () {
   browserSync.init(null,{
     proxy: 'http://localhost:7777',
-    port: 9001
+    port: config.port,
   });
 });
