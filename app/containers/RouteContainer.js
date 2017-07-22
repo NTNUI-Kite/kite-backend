2580eec6d6fd282db9aae4a607269d3095e16ccd
oleob <ole.barsch@gmail.com>
2017-07-22 16:19:44 +0200

    Moved routes to RouteContainer

--- app/containers/RouteContainer.js
+++ app/containers/RouteContainer.js
@@ -4,10 +4,10 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 
 import TestContainer from '../containers/TestContainer';
 
-const Routes = ({store}) => (
+const RouteContainer = ({store}) => (
   <HashRouter>
     <Route path = "/" component={TestContainer}/>
   </HashRouter>
 );
 
-export default Routes;
+export default RouteContainer;
--- app/main.js
+++ app/main.js
@@ -4,12 +4,12 @@ import ReactDOM from 'react-dom';
 //import HomePage from './components/common/header';
 //import Header from './components/HomePage';
 
-import Routes from './utilities/routes';
+import RouteContainer from './containers/RouteContainer';
 
 class App extends React.Component {
   render(){
     return(
-      <Routes/>
+      <RouteContainer/>
     );
   }
 }
