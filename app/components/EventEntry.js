f0d77f45e9b1c204a8e682e3476536589c434fc1
oleob <ole.barsch@gmail.com>
2017-07-23 14:52:51 +0200

    renamed to event

---
+++ app/components/EventEntry.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Card,CardHeader, CardMedia, CardText, CardTitle} from 'material-ui/Card';
+
+class TripEntry extends Component {
+  render(){
+    return(
+      <Card>
+        
+      </Card>
+    );
+  }
+}
--- app/components/TripEntry.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-class TripEntry extends Component {
-  render(){
-    return(
-
-    );
-  }
-}
