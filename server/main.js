95241ef7127d769807bdde97488795e0444f3761
oleob <ole.barsch@gmail.com>
2017-07-30 13:22:02 +0200

    created mock api for event

--- server/main.js
+++ server/main.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var Event =  require('./models/Event');
+
+// var mysql = require('mysql');
+//
+// var con = mysql.createConnection({
+//   host: '',
+//   user: '',
+//   password: '',
+//   database: ''
+// });
+//
+// con.connect();
 
 var app = new express();
 
 app.get('/', function (req,res) {
   res.render('./../app/index.ejs',{});
-})
-.use(express.static(__dirname + './../.dist'))
+}).use(express.static(__dirname + './../.dist'))
 .listen(7777);
+
+app.get('/api/allEvents',function(req,res){
+  res.json(Event.getAllEvents());
+});
