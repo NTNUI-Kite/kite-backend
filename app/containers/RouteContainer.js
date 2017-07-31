b549af2475a4cc7c42f74747235d0a86fd5544bf
oleob <ole.barsch@gmail.com>
2017-07-31 18:49:58 +0200

    fixed routing

--- app/containers/RouteContainer.js
+++ app/containers/RouteContainer.js
@@ -26,10 +26,10 @@ const RouteContainer = ({store}) => (
         <NavBarContainer/>
         <HashRouter>
           <Switch>
+            <Route exact path = "/" component={TestContainer}/>
             <Route path = "/board" component = {BoardContainer}/>
             <Route path = "/blog" component = {BlogContainer}/>
             <Route path = "/events" component = {EventContainer}/>
-            <Route path = "/" component={TestContainer}/>
           </Switch>
         </HashRouter>
         <Footer/>
