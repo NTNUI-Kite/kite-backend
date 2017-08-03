2b28eeea9e182aa752f7ef737dd4f26daeee7802
oleob <ole.barsch@gmail.com>
2017-08-03 15:19:57 +0200

    Added edit view and event preview

--- app/containers/EditEventContainer.js
+++ app/containers/EditEventContainer.js
@@ -1,13 +1,27 @@
 import React,  {Component} from 'react';
 import Card from 'material-ui/Card';
+import TextField from 'material-ui/TextField';
+import DatePicker from 'material-ui/DatePicker';
 import EditView from '../components/EditView';
+import EventEntry from '../components/EventEntry';
 
 class EditEventContainer extends Component {
   render(){
     return(
-      <Card className = "baseContainer">
-        <EditView/>
-      </Card>
+      <div className = "baseContainer">
+        <Card className = "fieldContainer">
+          <TextField className="fieldItem" hintText = "Tittel"/>
+          <TextField className="fieldItem" hintText = "Kapasitet"/>
+          <TextField className="fieldItem" hintText = "Pris" />
+          <DatePicker className="fieldItem" hintText="Start-dato" mode="landscape" />
+          <DatePicker className="fieldItem" hintText="Slutt-dato" mode="landscape" />
+          <DatePicker className="fieldItem" hintText="frist-dato" mode="landscape" />
+        </Card>
+        <Card className = "editContainer">
+          <EditView/>
+        </Card>
+        <EventEntry/>
+      </div>
     );
   }
 }
