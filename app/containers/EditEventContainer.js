72b5ac61dd8f8bc70af60008705287897b4bcd7d
oleob <ole.barsch@gmail.com>
2017-08-04 09:08:21 +0200

    Replaced card with paper

--- app/containers/EditEventContainer.js
+++ app/containers/EditEventContainer.js
@@ -1,5 +1,5 @@
 import React,  {Component} from 'react';
-import Card from 'material-ui/Card';
+import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 import DatePicker from 'material-ui/DatePicker';
 import EditView from '../components/EditView';
@@ -9,17 +9,17 @@ class EditEventContainer extends Component {
   render(){
     return(
       <div className = "baseContainer">
-        <Card className = "fieldContainer">
+        <Paper className = "fieldContainer">
           <TextField className="fieldItem" hintText = "Tittel"/>
           <TextField className="fieldItem" hintText = "Kapasitet"/>
           <DatePicker className="fieldItem" hintText="Start-dato" mode="landscape" />
           <DatePicker className="fieldItem" hintText="Slutt-dato" mode="landscape" />
           <DatePicker className="fieldItem" hintText="frist-dato" mode="landscape" />
           <TextField className="fieldItem" hintText = "Pris" />
-        </Card>
-        <Card className = "editContainer">
+        </Paper>
+        <Paper className = "editContainer">
           <EditView/>
-        </Card>
+        </Paper>
         <EventEntry/>
       </div>
     );
