c07361b89ceae5d9d617e1f1d4bd86ee30d4e781
Emil Schrøder <emilps@stud.ntnu.no>
2017-08-05 01:26:12 +0200

    Lagt til linker på bildene på about

--- app/components/AboutLinks.js
+++ app/components/AboutLinks.js
@@ -4,16 +4,17 @@ import EventIcon from 'material-ui/svg-icons/action/event';
 
 const PaperLinks = () => (
   <Paper className = "boardLinks">
-    
+    <a target="_blank" href='https://www.snapchat.com/add/ntnuikite'>
       <img className="AboutLinkIcon" src={'http://www.freeiconspng.com/uploads/142411756098-snapchat-icon-1.png'} />
-
+    </a>
+    <a target="_blank" href='https://www.facebook.com/groups/NTNUIKite'>
       <img className="AboutLinkIcon"
       src={'http://www.freeiconspng.com/uploads/circle-color-facebook-icon--6.png'} />
-
-
+    </a>
+    <a target="_blank" href='https://www.instagram.com/ntnuikite/'>
       <img className="AboutLinkIcon"
       src={'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2000px-Instagram_logo_2016.svg.png'} />
-
+    </a>
   </Paper>
 );
 
