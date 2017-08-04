a8e21f7ee00f2e7b8249f565905ca243af49245e
Emil Schrøder <emilps@stud.ntnu.no>
2017-08-05 01:14:30 +0200

    fixet litt style

--- app/components/AboutLinks.js
+++ app/components/AboutLinks.js
@@ -4,17 +4,16 @@ import EventIcon from 'material-ui/svg-icons/action/event';
 
 const PaperLinks = () => (
   <Paper className = "boardLinks">
-    <Paper className="boardLinkItem" zDepth={2} >
+    
       <img className="AboutLinkIcon" src={'http://www.freeiconspng.com/uploads/142411756098-snapchat-icon-1.png'} />
-    </Paper>
-    <Paper className="boardLinkItem" zDepth={2} >
+
       <img className="AboutLinkIcon"
       src={'http://www.freeiconspng.com/uploads/circle-color-facebook-icon--6.png'} />
-    </Paper>
-    <Paper className="boardLinkItem" zDepth={2} >
+
+
       <img className="AboutLinkIcon"
       src={'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2000px-Instagram_logo_2016.svg.png'} />
-    </Paper>
+
   </Paper>
 );
 
