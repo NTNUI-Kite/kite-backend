71371270d926f8a2c8f1ca8041a9191325c7f462
Emil Schrøder <emilps@stud.ntnu.no>
2017-08-05 01:09:19 +0200

    lagt til logo for some

--- app/components/AboutLinks.js
+++ app/components/AboutLinks.js
@@ -5,16 +5,15 @@ import EventIcon from 'material-ui/svg-icons/action/event';
 const PaperLinks = () => (
   <Paper className = "boardLinks">
     <Paper className="boardLinkItem" zDepth={2} >
-      <EventIcon className="boardLinkIcon"/>
-      <p>Arrangement</p>
+      <img className="AboutLinkIcon" src={'http://www.freeiconspng.com/uploads/142411756098-snapchat-icon-1.png'} />
     </Paper>
     <Paper className="boardLinkItem" zDepth={2} >
-      <EventIcon className="boardLinkIcon"/>
-      <p>Arrangement</p>
+      <img className="AboutLinkIcon"
+      src={'http://www.freeiconspng.com/uploads/circle-color-facebook-icon--6.png'} />
     </Paper>
     <Paper className="boardLinkItem" zDepth={2} >
-      <EventIcon className="boardLinkIcon"/>
-      <p>Arrangement</p>
+      <img className="AboutLinkIcon"
+      src={'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2000px-Instagram_logo_2016.svg.png'} />
     </Paper>
   </Paper>
 );
