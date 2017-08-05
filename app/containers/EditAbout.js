5ca5e99fb8c6bf2f6595963ebdfb4e0f26caac22
Emil Schrøder <emilps@stud.ntnu.no>
2017-08-05 12:43:03 +0200

    lagt til editor for aboutsiden

--- app/containers/EditAbout.js
+++ app/containers/EditAbout.js
@@ -1,11 +1,74 @@
 import React, {Component} from 'react';
+import Paper from 'material-ui/Paper';
 import {Card} from 'material-ui/Card'
+import TextField from 'material-ui/TextField';
+import DatePicker from 'material-ui/DatePicker';
 
+import { EditorState, convertToRaw, ContentState } from 'draft-js';
+import draftToHtml from 'draftjs-to-html';
+import htmlToDraft from 'html-to-draftjs';
+
+import EditView from '../components/EditView';
+
+import {getAboutInfo} from '../utilities/APIFunctions';
 
 class EditAbout extends Component{
-  constructor(props){
-    super(props)
+  constructor(){
+    super()
+    this.state = {
+      hasRecievedData: false
+    }
+
+    this.onEditorStateChange = this.onEditorStateChange.bind(this);
   }
+
+  onEditorStateChange(editorState){
+    this.setState({
+      editorState,
+    });
+  }
+/*
+  getData(){
+    getAboutInfo(0).then((res)=>{
+      const data = res.data;
+      const blocksFromHtml = htmlToDraft(data.abstract);
+      const content = ContentState.createFromBlockArray(blocksFromHtml);
+
+
+      this.setState({
+        name: data.name,
+        abstract: data.abstract,
+        start: new Date(data.start),
+        end: new Date(data.end),
+        capacity: data.capacity,
+        price: data.price,
+        registration: new Date(data.registration),
+        hasRecievedData:true,
+        editorState: EditorState.createWithContent(content),
+      });
+    });
+  }
+*/
+  getData(){
+    console.log("kommer hit")
+    getAboutInfo().then((res) =>{
+      console.log("1")
+      const data = res.data.info;
+      const blocksFromHtml = htmlToDraft(data.abstract);
+      const content = ContentState.createFromBlockArray(blocksFromHtml);
+      console.log("2")
+      this.setState({
+        title: data.title,
+        abstract: data.abstract,
+        editorState: EditorState.createWithContent(content)
+      })
+    })
+  }
+
+  componentDidMount(){
+    this.getData();
+  }
+
 
   render(){
     return(
@@ -16,6 +79,9 @@ class EditAbout extends Component{
             <strong>Soon to come</strong>
           </p>
         </Card>
+        <Paper className = "editContainer">
+          <EditView editorState = {this.state.editorState} onEditorStateChange = {this.onEditorStateChange}/>
+        </Paper>
       </div>
 
 
