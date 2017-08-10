b888016e6fcfab497ad57f186cb92c1d75609d96
oleob <ole.barsch@gmail.com>
2017-08-10 15:56:30 +0200

    Testing flux on test-container

--- app/containers/TestContainer.js
+++ app/containers/TestContainer.js
@@ -1,6 +1,46 @@
 import React, {Component} from 'react';
 
+import EventActions from '../actions/EventActions';
+import EventStore from '../stores/EventStore';
+
 class TestContainer extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      event: {},
+      events: []
+    }
+    this.onChange = this.onChange.bind(this);
+  }
+
+  componentWillMount(){
+    EventStore.addChangeListener(this.onChange);
+  }
+
+  componentDidMount(){
+    EventActions.getEvent(0); //TODO replace with param
+    EventActions.recieveEvents();
+  }
+
+  componentWillUnmount(){
+    EventStore.removeChangeListener(this.onChange);
+  }
+
+  componentWillReceiveProps(nextProps){
+    this.setState({
+      event: EventActions.getEvent(0), //TODO replace with param
+      events: EventActions.recieveEvents()
+    })
+  }
+
+  onChange(){
+    this.setState({
+      event: EventStore.getEvent(),
+      events: EventStore.getEvents()
+    });
+  }
+
   render() {
     return(
       <h1>Hello</h1>
