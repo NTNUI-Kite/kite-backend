c09fb4f5627bf536dc3c97e4815b4256900a358b
oleob <ole.barsch@gmail.com>
2017-08-10 16:59:23 +0200

    Added username to navigation bar

--- app/components/NavigationBar.js
+++ app/components/NavigationBar.js
@@ -6,44 +6,79 @@ import AuthStore from '../stores/AuthStore';
 
 const logo = <div className="logo"><img src="http://via.placeholder.com/167x100" /></div>;
 
+const loginButton = (authenticated,login,logout,userInfo) =>{
+  if(!authenticated){
+    return(
+      <div>
+        <FlatButton onClick={login}>Login</FlatButton>
+      </div>
+    );
+  }
+  else{
+    return(
+      <div>
+        <p>{userInfo.name}</p>
+        <FlatButton onClick={logout}>Logout</FlatButton>
+      </div>
+    );
+  }
+}
+
 class NavigationBar extends Component {
 
   constructor() {
-      super();
-      this.state = {
-        authenticated: false
+    super();
+    this.state = {
+      authenticated: false,
+      userInfo: {}
+    }
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  login() {
+    this.props.lock.show((err, profile, token) => {
+      if (err) {
+        alert(err);
+        return;
       }
-      this.login = this.login.bind(this);
-      this.logout = this.logout.bind(this);
+      AuthActions.logUserIn(profile, token);
+      this.setState(
+        {
+          authenticated: true,
+          userInfo: JSON.parse(AuthStore.getUser())
+        });
+      });
+    }
+  logout() {
+      AuthActions.logUserOut();
+      this.setState(
+        {
+          authenticated: false,
+          userInfo: {}
+        });
+  }
+
+  componentDidMount(){
+    const authenticated = AuthStore.isAuthenticated;
+    if(authenticated){
+      this.setState({
+        userInfo: JSON.parse(AuthStore.getUser()),
+        authenticated: AuthStore.isAuthenticated()
+      });
+    }
+    else{
+      this.setState({
+        authenticated: AuthStore.isAuthenticated()
+      });
     }
 
-    login() {
-      this.props.lock.show((err, profile, token) => {
-        if (err) {
-          alert(err);
-          return;
-        }
-        AuthActions.logUserIn(profile, token);
-        this.setState({authenticated: true});
-        });
-      }
-
-      logout() {
-        AuthActions.logUserOut();
-        this.setState({authenticated: false});
-      }
+  }
 
   render(){
     return(
       <AppBar className = "navigationBar" iconElementLeft = {logo}
-        iconElementRight=
-        { !AuthStore.isAuthenticated() ? (
-            <FlatButton onClick={this.login}>Login</FlatButton>
-          ) : (
-            <FlatButton onClick={this.logout}>Logout</FlatButton>
-          )
-        }
-      />
+        iconElementRight= {loginButton(this.state.authenticated,this.login,this.logout,this.state.userInfo)}/>
     );
   }
 }
