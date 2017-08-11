a616d1a11a41924929386f40447cdf79377c49c8
oleob <ole.barsch@gmail.com>
2017-08-11 13:25:44 +0200

    fun in testcontainer

--- app/containers/TestContainer.js
+++ app/containers/TestContainer.js
@@ -3,45 +3,47 @@ import React, {Component} from 'react';
 import EventActions from '../actions/EventActions';
 import EventStore from '../stores/EventStore';
 
+import BlogActions from '../actions/BlogActions';
+import BlogStore from '../stores/BlogStore';
+
 class TestContainer extends Component {
 
   constructor(){
     super();
     this.state = {
       event: {},
-      events: []
+      events: [],
+      posts: []
     }
     this.onChange = this.onChange.bind(this);
   }
 
   componentWillMount(){
-    EventStore.addChangeListener(this.onChange);
+    //EventStore.addChangeListener(this.onChange);
+    BlogStore.addChangeListener(this.onChange);
   }
 
   componentDidMount(){
-    EventActions.getEvent(0); //TODO replace with param
-    EventActions.recieveEvents();
+    //EventActions.getEvent(0); //TODO replace with param
+    //EventActions.getEvents();
+    BlogActions.getPosts();
   }
 
   componentWillUnmount(){
-    EventStore.removeChangeListener(this.onChange);
-  }
-
-  componentWillReceiveProps(nextProps){
-    this.setState({
-      event: EventActions.getEvent(0), //TODO replace with param
-      events: EventActions.recieveEvents()
-    })
+    //EventStore.removeChangeListener(this.onChange);
+    BlogStore.removeChangeListener(this.onChange);
   }
 
   onChange(){
     this.setState({
-      event: EventStore.getEvent(),
-      events: EventStore.getEvents()
+      //event: EventStore.getEvent(),
+      //events: EventStore.getEvents(),
+      posts: BlogStore.getPosts()
     });
   }
 
   render() {
+    console.log(this.state);
     return(
       <h1>Hello</h1>
     );
