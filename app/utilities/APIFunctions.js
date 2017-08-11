23a4e0901af7e940193add19451a7c66befb5bdc
oleob <ole.barsch@gmail.com>
2017-08-11 15:09:48 +0200

    Removed unused functions

--- app/utilities/APIFunctions.js
+++ app/utilities/APIFunctions.js
@@ -1,14 +1,5 @@
-import axios from 'axios';
 import request from 'superagent/lib/client';
 import AuthStore from '../stores/AuthStore';
-
-export function getBoardMembers(){
-  return fetchFromServer("/api/boardMembers");
-}
-
-export function getInstaFeed(){
-  return fetchFromServer("/api/instaFeed");
-}
 
 export function authorizedGetRequest(url){
   return new Promise((resolve, reject) => {
@@ -31,22 +22,4 @@ export function getRequest(url){
       resolve(JSON.parse(response.text));
     })
   });
-}
-
-function fetchFromServer(query) {
-    return axios.get(query, {
-        credentials: "same-origin"
-    }).then(response => {
-        if (response.status >= 400) {
-            throw new Error("GET-request: Bad response from server");
-        }
-        return response
-    }).then(function (result) {
-        if (result.error) {
-            console.log("The GET-request threw an error:\n" + query);
-            return Promise.reject(result.error);
-        } else {
-            return result;
-        }
-    });
 }
