c97c962fee41ad8ceef34c21e75f435f1280e559
oleob <oleob@stud.ntnu.no>
2017-08-23 20:52:19 +0200

    fixed editEventContainer

--- app/containers/EditEventContainer.js
+++ app/containers/EditEventContainer.js
@@ -10,7 +10,8 @@ import htmlToDraft from 'html-to-draftjs';
 import EditView from '../components/EditView';
 import EventEntry from '../components/EventEntry';
 
-import {getEventById} from '../utilities/APIFunctions';
+import EventActions from '../actions/EventActions';
+import EventStore from '../stores/EventStore';
 
 class EditEventContainer extends Component {
 
@@ -26,6 +27,28 @@ class EditEventContainer extends Component {
     this.handleDateEndChange = this.handleDateEndChange.bind(this);
     this.handleDateRegistrationChange = this.handleDateRegistrationChange.bind(this);
     this.onEditorStateChange = this.onEditorStateChange.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  componentWillMount(){
+    EventStore.addChangeListener(this.onChange);
+  }
+
+  componentWillUnmount(){
+    EventStore.removeChangeListener(this.onChange);
+  }
+
+  componentDidMount(){
+    EventActions.getEvent(this.props.eventId);
+  }
+
+  onChange(){
+    const event = EventStore.getEvent();
+    event.hasRecievedData = true;
+    const blocksFromHtml = htmlToDraft(event.abstract);
+    const content = ContentState.createFromBlockArray(blocksFromHtml);
+    event.editorState = EditorState.createWithContent(content);
+    this.setState(event);
   }
 
   onEditorStateChange(editorState){
@@ -34,30 +57,26 @@ class EditEventContainer extends Component {
     });
   }
 
-  getData(){
-    getEventById(0).then((res)=>{
-      const data = res.data;
-      const blocksFromHtml = htmlToDraft(data.abstract);
-      const content = ContentState.createFromBlockArray(blocksFromHtml);
-
-
-      this.setState({
-        name: data.name,
-        abstract: data.abstract,
-        start: new Date(data.start),
-        end: new Date(data.end),
-        capacity: data.capacity,
-        price: data.price,
-        registration: new Date(data.registration),
-        hasRecievedData:true,
-        editorState: EditorState.createWithContent(content),
-      });
-    });
-  }
-
-  componentDidMount(){
-    this.getData();
-  }
+  // getData(){
+  //   getEventById(0).then((res)=>{
+  //     const data = res.data;
+  //     const blocksFromHtml = htmlToDraft(data.abstract);
+  //     const content = ContentState.createFromBlockArray(blocksFromHtml);
+  //
+  //
+  //     this.setState({
+  //       name: data.name,
+  //       abstract: data.abstract,
+  //       start: new Date(data.start),
+  //       end: new Date(data.end),
+  //       capacity: data.capacity,
+  //       price: data.price,
+  //       registration: new Date(data.registration),
+  //       hasRecievedData:true,
+  //       editorState: EditorState.createWithContent(content),
+  //     });
+  //   });
+  // }
 
   handleChange({target}) {
       this.setState({
@@ -90,21 +109,19 @@ class EditEventContainer extends Component {
     if(!this.state.hasRecievedData){
       return(<div></div>);
     }
-
     return(
       <div className = "baseContainer">
         <Paper className = "fieldContainer">
           <TextField className="fieldItem" name = "name" floatingLabelText = "Tittel" defaultValue = {this.state.name} onChange = {this.handleChange}/>
           <TextField className="fieldItem" name = "capacity" floatingLabelText = "Kapasitet" defaultValue = {this.state.capacity} onChange = {this.handleChange}/>
-          <DatePicker className="fieldItem" name = "start" floatingLabelText="Start-dato" mode="landscape" value = {this.state.start} onChange = {this.handleDateStartChange}/>
-          <DatePicker className="fieldItem" name = "end" floatingLabelText="Slutt-dato" mode="landscape" value = {this.state.end} onChange = {this.handleDateEndChange}/>
-          <DatePicker className="fieldItem" name = "registration" floatingLabelText="frist-dato" mode="landscape" value = {this.state.registration} onChange = {this.handleDateRegistrationChange}/>
+          <DatePicker className="fieldItem" name = "start" floatingLabelText="Start-dato" mode="landscape" value = {new Date(this.state.start)} onChange = {this.handleDateStartChange}/>
+          <DatePicker className="fieldItem" name = "end" floatingLabelText="Slutt-dato" mode="landscape" value = {new Date(this.state.end)} onChange = {this.handleDateEndChange}/>
+          <DatePicker className="fieldItem" name = "registration" floatingLabelText="frist-dato" mode="landscape" value = {new Date(this.state.registration)} onChange = {this.handleDateRegistrationChange}/>
           <TextField className="fieldItem" name = "price" floatingLabelText = "Pris" defaultValue = {this.state.price} onChange = {this.handleChange}/>
         </Paper>
         <Paper className = "editContainer">
           <EditView editorState = {this.state.editorState} onEditorStateChange = {this.onEditorStateChange}/>
         </Paper>
-        <EventEntry abstract = {draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()))}/>
       </div>
     );
   }
