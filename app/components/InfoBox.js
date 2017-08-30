5ba613680f67d4d0d429949e2a499aec74d741cf
oleob <oleob@stud.ntnu.no>
2017-08-30 13:31:00 +0200

    Changed from component to const and made it use spots_available

--- app/components/InfoBox.js
+++ app/components/InfoBox.js
@@ -3,29 +3,26 @@ import Paper from 'material-ui/Paper';
 import Divider from 'material-ui/Divider';
 
 
-class InfoBox extends Component {
+const InfoBox = (event) => {
 
-  constructor(){
-    super();
+  if(!event){
+    return(<Paper className="infoBox"/>)
   }
 
-
-  render(){ //TODO: fix frist
-    return(
-      <Paper className = "infoBox">
-        <h3>Info</h3>
-        <p>Fra: {this.props.start}</p>
-        <Divider/>
-        <p>Til: {this.props.end}</p>
-        <Divider/>
-        <p>Frist: {this.props.start}</p>
-        <Divider/>
-        <p>Plasser: 0/{this.props.capacity}</p>
-        <Divider/>
-        <p>Pris: {this.props.price}</p>
-      </Paper>
-    );
-  }
+  return(
+    <Paper className = "infoBox">
+      <h3>Info</h3>
+      <p>Fra: {event.start}</p>
+      <Divider/>
+      <p>Til: {event.end}</p>
+      <Divider/>
+      <p>Frist: {event.start}</p>
+      <Divider/>
+      <p>Plasser: {event.spots_taken}/{event.capacity}</p>
+      <Divider/>
+      <p>Pris: {event.price}</p>
+    </Paper>
+  );
 }
 
-export default InfoBox; 
+export default InfoBox;
