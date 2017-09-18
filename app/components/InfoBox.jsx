07f5a583a7f69ac0c5997cf1a92957c2e890cd09
oleob <ole.barsch@gmail.com>
2017-09-18 22:31:12 +0200

    beautified infobox and fixed reserved

--- app/components/InfoBox.jsx
+++ app/components/InfoBox.jsx
@@ -2,22 +2,30 @@ import React from 'react';
 import Paper from 'material-ui/Paper';
 import Divider from 'material-ui/Divider';
 
+const createDate = (mysqlDate) => {
+  const dateParts = mysqlDate.split('-');
+  return new Date(dateParts[0], dateParts[1] - 1, dateParts[2].substr(0, 2));
+};
 
 const InfoBox = (event) => {
-  if (!event) {
+  if (!event || !event.start) {
     return (<Paper className="infoBox" />);
   }
+
+  const start = createDate(event.start);
+  const end = createDate(event.end);
+  const deadline = createDate(event.deadline);
 
   return (
     <Paper className="infoBox">
       <h3>Info</h3>
-      <p>Fra: {event.start}</p>
+      <p>Fra: {start.toDateString()}</p>
       <Divider />
-      <p>Til: {event.end}</p>
+      <p>Til: {end.toDateString()}</p>
       <Divider />
-      <p>Frist: {event.start}</p>
+      <p>Frist: {deadline.toDateString()}</p>
       <Divider />
-      <p>Plasser: {event.spots_taken}/{event.capacity}</p>
+      <p>Plasser tatt: {event.spotsTaken}/{event.capacity}</p>
       <Divider />
       <p>Pris: {event.price}</p>
     </Paper>
