f8810857ddbee5e9964b87ebe2c457a5b2ca54f6
oleob <ole.barsch@gmail.com>
2017-09-21 12:57:09 +0200

    Moved login button to seperate file

--- app/components/NavigationBar.jsx
+++ app/components/NavigationBar.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { AppBar, FlatButton } from 'material-ui';
+import { AppBar } from 'material-ui';
 import PropTypes from 'prop-types';
 import { Link, withRouter } from 'react-router-dom';
+
+import LoginButton from './LoginButton';
 
 import AuthActions from '../actions/AuthActions';
 
@@ -35,23 +37,6 @@ const navLinks = (boardMember) => {
   );
 };
 
-const loginButton = (authenticated, login, logout, userInfo) => {
-  if (!authenticated) {
-    return (
-      <div>
-        <FlatButton onClick={login}>Login</FlatButton>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <p>{userInfo.name}</p>
-      <FlatButton onClick={logout}>Logout</FlatButton>
-    </div>
-  );
-};
-
 class NavigationBar extends Component {
   constructor(props) {
     super(props);
@@ -79,8 +64,14 @@ class NavigationBar extends Component {
         className="navigationBar"
         iconElementLeft={logo}
         title={navLinks(this.props.boardMember)}
-        iconElementRight={loginButton(this.props.authenticated,
-          this.login, this.logout, this.props.userInfo)}
+        iconElementRight={
+          <LoginButton
+            authenticated={this.props.authenticated}
+            login={this.login}
+            logout={this.logout}
+            userInfo={this.props.userInfo}
+          />
+        }
       />
     );
   }
