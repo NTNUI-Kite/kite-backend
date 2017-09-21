d429c5014e3c4a56492b2a2824d456e284f1105c
oleob <ole.barsch@gmail.com>
2017-09-21 12:55:59 +0200

    Added dialog for editing userinfo

---
+++ app/components/EditUserDialog.jsx
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import Dialog from 'material-ui/Dialog';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+import PropTypes from 'prop-types';
+import AuthActions from '../actions/AuthActions';
+
+class EditUserDialog extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: props.userInfo.name,
+      phone: props.userInfo.phone, // TODO: replace this
+      email: props.userInfo.email,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.updateUser = this.updateUser.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
+  updateUser() {
+    const userObject = {
+      id: this.props.userInfo.id,
+      facebook_id: this.props.userInfo.facebook_id,
+      name: this.state.name,
+      phone: this.state.phone,
+      email: this.state.email,
+    };
+
+    AuthActions.updateUser(userObject);
+    this.props.toggle();
+  }
+
+  render() {
+    const actions = [
+      <FlatButton
+        key={0}
+        label="Cancel"
+        primary={true}
+        keyboardFocused={true}
+        onClick={this.props.toggle}
+      />,
+      <FlatButton
+        key={1}
+        label="Save"
+        primary={true}
+        keyboardFocused={true}
+        onClick={this.updateUser}
+      />,
+    ];
+
+    return (
+      <div>
+        <Dialog
+          title="User profile"
+          actions={actions}
+          modal={false}
+          open={this.props.open}
+          onRequestClose={this.handleClose}
+        >
+          <TextField
+            name="name"
+            hintText="Name"
+            floatingLabelText="Name"
+            defaultValue={this.state.name}
+            onChange={this.handleChange}
+          />
+          <TextField
+            name="phone"
+            hintText="Phone-number"
+            floatingLabelText="Phone-number"
+            defaultValue={this.state.phone}
+            onChange={this.handleChange}
+          />
+          <TextField
+            name="email"
+            hintText="Email"
+            floatingLabelText="Email"
+            defaultValue={this.state.email}
+            onChange={this.handleChange}
+          />
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+EditUserDialog.propTypes = {
+  toggle: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+  userInfo: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    phone: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default EditUserDialog;
