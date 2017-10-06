930271ded08a87ef5fea8b9248ef2a0ee2762de2
oleob <oleob@stud.ntnu.no>
2017-10-06 20:33:18 +0200

    Set up payment for use in singleEventContainer

--- server/models/Payment.js
+++ server/models/Payment.js
@@ -1,21 +1,35 @@
 import configureStripe from 'stripe';
 import stripeSecret from '../config/StripeConfig';
+import db from '../utilities/dbConnection';
+import Event from './Event';
 
+const CURRENCY = 'NOK';
 
 const stripe = configureStripe(stripeSecret);
 
-const postStripeCharge = res => (stripeErr, stripeRes) => {
+const postStripeCharge = (req, res) => (stripeErr, stripeRes) => {
   if (stripeErr) {
     res.status(500).send({ error: stripeErr });
   } else {
-    res.status(200).send({ success: stripeRes });
+    // res.status(200).send({ success: stripeRes });
+    Event.signup(req, res);
   }
 };
 
 const Payment = {
   pay: (req, res) => {
-    console.log(req.body);
-    stripe.charges.create(req.body, postStripeCharge(res));
+    db.query('SELECT title, price FROM events WHERE id = ?', [req.body.eventId], (err, rows) => {
+      if (err) throw err;
+
+      const event = rows[0];
+      const payment = {
+        description: event.title,
+        source: req.body.source,
+        currency: CURRENCY,
+        amount: event.price * 100,
+      };
+      stripe.charges.create(payment, postStripeCharge(req, res));
+    });
   },
 };
 
