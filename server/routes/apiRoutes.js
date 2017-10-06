9e5e2e2ea7010ea1f24120012b70fa994bb1aa53
oleob <oleob@stud.ntnu.no>
2017-10-06 17:14:40 +0200

    Added payment endpoint

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -5,6 +5,7 @@ import Blog from '../models/Blog';
 import Board from '../models/Board';
 import User from '../models/User';
 import About from '../models/About';
+import Payment from '../models/Payment';
 
 import getInstaFeed from '../utilities/InstaScraper';
 import Security from '../utilities/Security';
@@ -64,6 +65,10 @@ router.get('/instaFeed', (req, res) => {
 
 router.post('/login', auth0Check, (req, res) => {
   User.Login(req.body, req.user.sub, res);
+});
+
+router.post('/pay', (req, res) => {
+  Payment.pay(req, res);
 });
 
 export default router;
