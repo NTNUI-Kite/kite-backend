560c3df1b56fc661763f2f197d608d2fb2f612e0
oleob <oleob@stud.ntnu.no>
2017-10-06 20:32:43 +0200

    Added loading animation

---
+++ app/components/baseComponents/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader = () => (
+  <div className="spinner">
+    <div className="bounce1" />
+    <div className="bounce2" />
+    <div className="bounce3" />
+  </div>
+);
+
+export default Loader;
