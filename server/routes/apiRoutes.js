93bbdd6613843884de442f34a1480fbd59e43c17
oleob <oleob@stud.ntnu.no>
2017-10-06 20:33:51 +0200

    Added authCheck to payment

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -33,7 +33,7 @@ router.post('/updateEvent', (req, res) => {
 });
 
 router.post('/eventSignup', authCheck, (req, res) => {
-  Event.signup(req.user.userId, req.body, res);
+  Event.signup(req, res);
 });
 
 router.post('/eventSignoff', authCheck, (req, res) => {
@@ -67,7 +67,7 @@ router.post('/login', auth0Check, (req, res) => {
   User.Login(req.body, req.user.sub, res);
 });
 
-router.post('/pay', (req, res) => {
+router.post('/pay', authCheck, (req, res) => {
   Payment.pay(req, res);
 });
 
