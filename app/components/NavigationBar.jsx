acc475ac9af9af5d8cede4619e101af5ae31c48c
oleob <oleob@stud.ntnu.no>
2017-10-07 12:07:04 +0200

    Removed unused code and used new components

--- app/components/NavigationBar.jsx
+++ app/components/NavigationBar.jsx
@@ -2,49 +2,16 @@ import React from 'react';
 import { AppBar } from 'material-ui';
 import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle } from 'material-ui/Toolbar';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 
 import LoginButton from './baseComponents/LoginButton';
 import LoggedInButton from './baseComponents/LoggedInButton';
+import NavLinks from './NavLinks';
+import Logo from './Logo';
 
-const logo = (
-  <div className="logo">
-    <Link to="/">
-      <img alt="logo" src="http://via.placeholder.com/167x64" />
-    </Link>
-  </div>
-);
-
-const navLinks = (boardMember) => {
-  if (boardMember) {
-    return (
-      <div>
-        <Link className="navLink" to="/events">Events</Link>
-        <Link className="navLink" to="/blog">Blog</Link>
-        <Link className="navLink" to="/about">About</Link>
-        <Link className="navLink" to="/images">Images</Link>
-        <Link className="navLink" to="/board">Board</Link>
-      </div>
-    );
-  }
-  return (
-    <div>
-      <Link className="navLink" to="/events">Events</Link>
-      <Link className="navLink" to="/blog">Blog</Link>
-      <Link className="navLink" to="/about">About</Link>
-      <Link className="navLink" to="/images">Images</Link>
-    </div>
-  );
-};
-
-const NavigationBar2 = props => (
+const NavigationBar = props => (
   <Toolbar className="navigationBar">
-    <ToolbarGroup firstChild>
-      {logo}
-    </ToolbarGroup>
-    <ToolbarGroup>
-      {navLinks(props.boardMember)}
-    </ToolbarGroup>
+    <Logo />
+    <NavLinks {...props.boardMember} />
     <ToolbarGroup>
       {props.authenticated
         ? <LoggedInButton userInfo={props.userInfo} />
@@ -53,24 +20,10 @@ const NavigationBar2 = props => (
   </Toolbar>
 );
 
-const NavigationBar = props => (
-  <AppBar
-    className="navigationBar"
-    iconElementLeft={logo}
-    title={navLinks(props.boardMember)}
-    iconElementRight={props.authenticated
-      ? <LoggedInButton userInfo={props.userInfo} />
-      : <LoginButton />}
-    iconStyleLeft={{
-      margin: 0,
-    }}
-  />
-);
-
 NavigationBar.propTypes = {
   boardMember: PropTypes.bool.isRequired,
   authenticated: PropTypes.bool.isRequired,
   userInfo: PropTypes.shape({}).isRequired,
 };
 
-export default NavigationBar2;
+export default NavigationBar;
