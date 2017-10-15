72433aa2c02ab93578371d4a14d16b3b996842fa
oleob <oleob@stud.ntnu.no>
2017-10-15 18:59:39 +0200

    Added endpoint for refreshing token

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -64,11 +64,15 @@ router.get('/instaFeed', (req, res) => {
 });
 
 router.post('/login', auth0Check, (req, res) => {
-  User.Login(req.body, req.user.sub, res);
+  User.login(req.body, req.user.sub, res);
 });
 
 router.post('/pay', authCheck, (req, res) => {
   Payment.pay(req, res);
+});
+
+router.get('/newToken', (req, res) => {
+  User.refreshToken(req, res);
 });
 
 export default router;
