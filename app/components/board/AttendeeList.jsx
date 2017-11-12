ede7782b7ff1b4fb4f1eb44c378dc37fa451ba33
oleob <oleob@stud.ntnu.no>
2017-11-12 16:45:03 +0100

    Add attendee list

---
+++ app/components/board/AttendeeList.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
+import Button from 'material-ui/RaisedButton';
+
+const AttendeeList = (props) => {
+  return (
+    <Table
+      selectable={false}
+    >
+      <TableHeader
+        displaySelectAll={false}
+        adjustForCheckbox={false}
+      >
+        <TableRow>
+          <TableHeaderColumn>Navn</TableHeaderColumn>
+          <TableHeaderColumn>Email</TableHeaderColumn>
+          <TableHeaderColumn>Tlf</TableHeaderColumn>
+          <TableHeaderColumn>Kommentar</TableHeaderColumn>
+          <TableHeaderColumn />
+        </TableRow>
+      </TableHeader>
+      <TableBody displayRowCheckbox={false}>
+        {
+          props.signups.map(member => (
+            <TableRow key={member.user_id}>
+              <TableRowColumn>{member.name}</TableRowColumn>
+              <TableRowColumn>{member.email}</TableRowColumn>
+              <TableRowColumn>{member.phone}</TableRowColumn>
+              <TableRowColumn style={{
+                whiteSpace: 'normal',
+                wordWrap: 'break-word',
+              }}
+              >{member.comment}</TableRowColumn>
+              <TableRowColumn>
+                <Button label="Remove" />
+              </TableRowColumn>
+            </TableRow>
+          ))
+        }
+      </TableBody>
+    </Table>
+  );
+};
+
+export default AttendeeList;
