eedba1b014afb2a7bc7baae4a8c3770530b32c83
oleob <oleob@stud.ntnu.no>
2017-11-18 12:24:02 +0100

    Fixed to comply with multiple changes

--- app/stores/BoardStore.js
+++ app/stores/BoardStore.js
@@ -109,10 +109,7 @@ BoardStore.dispatchToken = AppDispatcher.register((action) => {
     case BoardConstants.UPDATE_EVENT:
       events = events.map((ev) => {
         if (ev.id === action.event.id) {
-          const newEvent = Object.assign({}, ev);
-          newEvent.is_active = action.event.is_active;
-          // setEvent(newEvent);
-          return newEvent;
+          return action.event;
         }
         return ev;
       });
