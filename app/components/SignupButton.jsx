8afad339dbae109103c0f942a3e8482cfef60079
oleob <ole.barsch@gmail.com>
2018-01-02 15:45:41 +0100

    Add logic for different occasions

--- app/components/SignupButton.jsx
+++ app/components/SignupButton.jsx
@@ -8,6 +8,13 @@ import EventActions from '../actions/EventActions';
 
 import LoginButton from './baseComponents/LoginButton';
 
+const signup = (body) => {
+  EventActions.signup(body)
+    .then(() => {
+      EventActions.getEvent(body.eventId);
+    });
+};
+
 const signOut = (eventId) => {
   EventActions.signoff({ eventId })
     .then(() => {
@@ -42,25 +49,52 @@ class SignupButton extends Component {
 
     if (this.props.authenticated) {
       if (this.props.hasSignedUp) {
+        if (this.props.hasPaid) {
+          // If authenticated, event is open, the user has signed up and has paid.
+          return (
+            <div>
+              <p>You are signed up</p>
+              <Button label="Sign off" onClick={this.toggleDialog} />
+              <ConfirmPopup title="Warning" open={this.state.openDialog} toggle={this.toggleDialog} continueAction={() => signOut(this.props.eventId)} text="Are you sure you want to sign off this event?" />
+            </div>
+          );
+        } else if (!this.props.onWaitingList) {
+          // If the user is signed up, but has not payed but is also not on the waiting list
+          return (
+            <div>
+              <p>You have not payed yet</p>
+              <Checkout
+                eventId={this.props.eventId}
+                description={this.props.description}
+                amount={this.props.price}
+                comment={this.props.comment}
+                hasCar={this.props.hasCar}
+                changePaymentProgress={this.props.changePaymentProgress}
+              />
+              <Button label="Sign off" onClick={this.toggleDialog} />
+              <ConfirmPopup title="Warning" open={this.state.openDialog} toggle={this.toggleDialog} continueAction={() => signOut(this.props.eventId)} text="Are you sure you want to sign off this event?" />
+            </div>
+          );
+        }
+        // If the user is signed up on the waiting list
         return (
           <div>
-            <p>You are signed up</p>
+            <p>You are on the waiting list</p>
             <Button label="Sign off" onClick={this.toggleDialog} />
             <ConfirmPopup title="Warning" open={this.state.openDialog} toggle={this.toggleDialog} continueAction={() => signOut(this.props.eventId)} text="Are you sure you want to sign off this event?" />
           </div>
         );
       }
+      // If the user is not signed up
+      const body = {
+        eventId: this.props.eventId,
+        comment: this.props.comment,
+        hasCar: this.props.hasCar,
+      };
       return (
         <div>
           <p>You are not signed up</p>
-          <Checkout
-            eventId={this.props.eventId}
-            description={this.props.description}
-            amount={this.props.price}
-            comment={this.props.comment}
-            hasCar={this.props.hasCar}
-            changePaymentProgress={this.props.changePaymentProgress}
-          />
+          <Button label="Sign up" onClick={() => signup(body)} />
         </div>
       );
     }
