f12ec4e539b4c4b1ca90a2a5faaddbf2f4c759bb
oleob <oleob@stud.ntnu.no>
2018-01-20 02:18:39 +0100

    Add send email on recieved payment

--- server/models/Payment.js
+++ server/models/Payment.js
@@ -1,17 +1,29 @@
 import configureStripe from 'stripe';
 import stripeSecret from '../config/StripeConfig';
 import db from '../utilities/dbConnection';
+import Emailer from '../utilities/Emailer';
 
 const CURRENCY = 'NOK';
 
 const stripe = configureStripe(stripeSecret);
 
+const createEmail = (name, email) => {
+  return {
+    email,
+    subject: 'Payment recieved',
+    content: `<h3>Thanks for the payment ${name} and welcome to the trip!</h3><p>Best regards</p></p>NTNUI-kite</p>`,
+  };
+};
+
 const postStripeCharge = (req, res) => (stripeErr, stripeRes) => {
   if (stripeErr) {
     res.status(500).send({ error: stripeErr });
   } else {
-    db.query('UPDATE event_signups SET has_paid = 1 WHERE event_id = ? AND user_id = ?', [req.body.eventId, req.user.userId], (err) => {
+    db.query('UPDATE event_signups SET has_paid = 1 WHERE event_id = ? AND user_id = ?; SELECT email FROM users WHERE id = ?', [req.body.eventId, req.user.userId, req.user.userId], (err, rows) => {
       if (err) throw err;
+      console.log(rows[1][0].email);
+      const emailBody = createEmail(req.user.name, rows[1][0].email);
+      Emailer.sendMail(emailBody);
       res.status(200).send({ success: stripeRes });
     });
     // req.body.has_paid = 1;
@@ -25,7 +37,7 @@ const Payment = {
       if (err) throw err;
       const event = rows[0];
       if (event.is_open === 0 || event.is_active === 0) {
-        res.status(500).send({ error: 'Event not open or active' });
+        res.status(500).send({ error: 'Event not open nor active' });
       } else {
         const payment = {
           description: `${event.title}: ${req.user.name}`,
