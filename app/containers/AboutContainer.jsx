b8b0b7473d6ed3c1ba54a861bb4b8cdf3830239a
Emil Schrøder <emilps@stud.ntnu.no>
2018-02-18 20:09:50 +0100

    Added boardmembers to aboutpage

--- app/containers/AboutContainer.jsx
+++ app/containers/AboutContainer.jsx
@@ -5,11 +5,17 @@ import AboutContent from '../components/AboutContent';
 import AboutActions from '../actions/AboutActions';
 import AboutStore from '../stores/AboutStore';
 
+import BoardList from '../components/board/BoardList';
+
+import BoardActions from '../actions/BoardActions';
+import BoardStore from '../stores/BoardStore';
+
 class AboutContainer extends Component {
   constructor() {
     super();
     this.state = {
       text: {},
+      boardMembers: [],
     };
     this.onChange = this.onChange.bind(this);
   }
@@ -20,24 +26,27 @@ class AboutContainer extends Component {
 
   componentDidMount() {
     AboutActions.getText();
+    BoardActions.getBoardMembers();
   }
 
   componentWillUnmount() {
     AboutStore.removeChangeListener(this.onChange);
+    BoardStore.removeChangeListener(this.onChange);
   }
 
   onChange() {
     this.setState({
       text: AboutStore.getText(),
+      boardMembers: BoardStore.getBoardMembers(),
     });
   }
 
   render() {
     return (
-      <div>
+      <div className="aboutContainer">
 
         <AboutContent informasjon={this.state.text} />
-
+        <BoardList members={this.state.boardMembers} />
       </div>);
   }
 }
