2fef2914e13c5220ddf70fe635fb9d867ac3a639
Emil Schrøder <emilps@stud.ntnu.no>
2018-03-05 20:05:09 +0100

    upcoming events design

--- app/components/EventPreviews.jsx
+++ app/components/EventPreviews.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import Divider from 'material-ui/Divider';
 import Paper from 'material-ui/Paper';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const EventPreviews = props => (
   <Paper className="eventPreviews" zDepth={2}>
     <h3>Upcoming events</h3>
-    {
-      props.events.map(event => (
-        <div key={event.id}>
-          <Divider />
-          <p>{event.title}</p>
-        </div>
-      ))
-    }
+    <table style={{ width: '100%' }}>
+      <tbody>
+        {
+          props.events.map(event => (
+            <tr className="eventPreview" key={event.id}>
+              <td>{event.title}</td>
+              <td>{event.start.split('-')[2].substr(0, 2)}.-{event.end.split('-')[2].substr(0, 2)}. {'0-january-february-march-april-may-june-july-august-september-october-november-desember'.split('-')[parseInt(event.end.split('-')[1], 10)]}</td>
+              <td>
+                <Link to={`/event/${event.id}`}>
+                  <p className="eventPreviewLink">Go to</p>
+                </Link>
+              </td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
   </Paper>
 );
 
