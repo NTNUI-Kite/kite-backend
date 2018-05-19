d957bbb9bc3a0f896056d483ee0f94981d1b651e
oleob <oleob@stud.ntnu.no>
2018-05-19 23:11:08 +0200

    remove app :'(

--- app/constants/AboutConstants.js
+++
@@ -1,8 +0,0 @@
-import keyMirror from 'keymirror';
-
-export default keyMirror({
-  RECEIVE_TEXT: null,
-  RECEIVE_TEXT_ERROR: null,
-  UPDATE_ABOUT: null,
-  UPDATE_ABOUT_ERROR: null,
-});
--- app/constants/InstaConstants.js
+++
@@ -1,6 +0,0 @@
-import keyMirror from 'keymirror';
-
-export default keyMirror({
-  RECIEVE_FEED: null,
-  RECIEVE_FEED_ERROR: null,
-});
