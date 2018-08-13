bec79c1b8e2773345df217709de438ebcf1403c2
Ole Barsch <ole@Oles-MacBook-Pro.local>
2018-08-13 11:18:36 +0200

    Fix SQL syntax

--- server/models/Event.js
+++ server/models/Event.js
@@ -43,7 +43,7 @@ const checkEventListIfOpen = (eventsList) => {
 const Event = {
   getActiveEvents(res, next) {
     db.query(
-      'DSELECT e.id, e.title, e.abstract, e.start, e.end, e.open, e.deadline, e.price, e.location, e.capacity, e.is_Open, IFNULL(es.spots_taken,0) as spots_taken FROM events AS e LEFT JOIN (select event_id, count(*) AS spots_taken FROM event_signups GROUP BY event_id) AS es ON e.id = es.event_id WHERE e.is_active = true AND e.end >= CURDATE()',
+      'SELECT e.id, e.title, e.abstract, e.start, e.end, e.open, e.deadline, e.price, e.location, e.capacity, e.is_Open, IFNULL(es.spots_taken,0) as spots_taken FROM events AS e LEFT JOIN (select event_id, count(*) AS spots_taken FROM event_signups GROUP BY event_id) AS es ON e.id = es.event_id WHERE e.is_active = true AND e.end >= CURDATE()',
       (err, rows) => {
         if (err) {
           next(err);
