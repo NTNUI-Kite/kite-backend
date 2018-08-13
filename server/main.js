ad8ec27cb9c8794eb73133e2e2f731182559230d
Ole Barsch <ole@Oles-MacBook-Pro.local>
2018-08-13 11:17:11 +0200

    change log info

--- server/main.js
+++ server/main.js
@@ -9,7 +9,7 @@ import boardRoutes from './routes/boardRoutes';
 const app = express();
 
 const errorHandler = (error, req, res, next) => {
-  console.log(error);
+  console.log('hello');
   response.status(500).json({ error: error.message });
 };
 
